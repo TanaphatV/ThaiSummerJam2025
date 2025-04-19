6d718f3e19b2250732d6e3a3453f8cf89fbb4fba
Sunny <sunny@bitegginc.com>
2025-04-19 21:45:07 +0700

    Move Mainmenu script to correct folder

---
+++ Assets/Script/MainMenu/MainMenuFunctions.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenuFunctions : MonoBehaviour
+{
+    public void PlayGame()
+    {
+        Debug.Log("Play Game button clicked");
+        // Load the game scene here
+    }
+}
