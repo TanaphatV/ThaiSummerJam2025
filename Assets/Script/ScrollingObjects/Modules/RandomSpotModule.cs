91bd5a93747d4708f5232e0819ec93576d88adce
Tanaphat Vivattanakijcharoen <tanaphatviva@gmail.com>
2025-04-25 13:52:53 +0700

    fix tumble weed

--- Assets/Script/ScrollingObjects/Modules/RandomSpotModule.cs
+++ Assets/Script/ScrollingObjects/Modules/RandomSpotModule.cs
@@ -13,9 +13,9 @@ public class RandomSpotModule : ScrollingObjectModule
 
         Vector2 halfSize = spawnZoneSize / 2.0f;
         Vector3 randomSpot = new Vector3(
-            transform.position.x + Random.Range(-halfSize.x,halfSize.x),
+            scrollingObject.physicalObject.transform.position.x + Random.Range(-halfSize.x,halfSize.x),
             scrollingObject.physicalObject.transform.position.y,
-            transform.position.z + Random.Range(-halfSize.y, halfSize.y)
+            scrollingObject.physicalObject.transform.position.z + Random.Range(-halfSize.y, halfSize.y)
             );
         scrollingObject.physicalObject.transform.position = randomSpot;
        
